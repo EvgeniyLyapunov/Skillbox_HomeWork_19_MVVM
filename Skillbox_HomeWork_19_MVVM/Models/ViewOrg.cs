db5fc7c82eb00c439102044b8de7c5d77df85d26
EgorTrubnikov <navakishor.108@gmail.com>
2021-06-05 20:31:11 +0300

    interface INotifyPropertyChanged  implementation in ViewPerson and ViewOrg classes

--- Skillbox_HomeWork_19_MVVM/Models/ViewOrg.cs
+++ Skillbox_HomeWork_19_MVVM/Models/ViewOrg.cs
@@ -3,24 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.ComponentModel;
 
 namespace Skillbox_HomeWork_19_MVVM.Models
 {
     public class ViewOrg: INotifyPropertyChanged
     {
+        public ViewOrg(int Id_Client, string Name, int EmployeeCount, decimal Deposit, decimal InterestRate)
+        {
+            id_Client = Id_Client;
+            name = Name;
+            employeeCount = EmployeeCount;
+            deposit = Deposit;
+            interestRate = InterestRate;
+        }
+
         private int id_Client;
         private string name;
         private int employeeCount;
         private decimal deposit;
         private decimal interestRate;
 
-        public int Id_Client { get => id_Client; set => id_Client = value; }
-        public string Name { get => name; set => name = value; }
-        public int EmployeeCount { get => employeeCount; set => employeeCount = value; }
-        public decimal Deposit { get => deposit; set => deposit = value; }
-        public decimal InterestRate { get => interestRate; set => interestRate = value; }
+        public int Id_Client
+        {
+            get => id_Client;
+            set
+            {
+                id_Client = value;
+                OnPropertyChanged("Id_Client");
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public int EmployeeCount
+        {
+            get => employeeCount;
+            set
+            {
+                employeeCount = value;
+                OnPropertyChanged("EmployeeCount");
+            }
+        }
+        public decimal Deposit
+        {
+            get => deposit;
+            set
+            {
+                deposit = value;
+                OnPropertyChanged("Deposit");
+            }
+        }
+        public decimal InterestRate
+        {
+            get => interestRate;
+            set
+            {
+                interestRate = value;
+                OnPropertyChanged("InterestRate");
+            }
+        }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
     }
 }
--- Skillbox_HomeWork_19_MVVM/Models/ViewPerson.cs
+++ Skillbox_HomeWork_19_MVVM/Models/ViewPerson.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.ComponentModel;
 
 namespace Skillbox_HomeWork_19_MVVM.Models
 {
     public class ViewPerson: INotifyPropertyChanged
     {
+        public ViewPerson(int Id_Client, string LastName, string FirstName, int Age, decimal Deposit, decimal InterestRate)
+        {
+            id_Client = Id_Client;
+            lastName = LastName;
+            firstName = FirstName;
+            age = Age;
+            deposit = Deposit;
+            interestRate = InterestRate;
+        }
+
         private int id_Client;
         private string lastName;
         private string firstName;
@@ -16,13 +27,69 @@ namespace Skillbox_HomeWork_19_MVVM.Models
         private decimal deposit;
         private decimal interestRate;
 
-        public int Id_Client { get => id_Client; set => id_Client = value; }
-        public string LastName { get => lastName; set => lastName = value; }
-        public string FirstName { get => firstName; set => firstName = value; }
-        public int Age { get => age; set => age = value; }
-        public decimal Deposit { get => deposit; set => deposit = value; }
-        public decimal InterestRate { get => interestRate; set => interestRate = value; }
+        public int Id_Client
+        {
+            get => id_Client;
+            set
+            {
+                id_Client = value;
+                OnPropertyChanged("Id_Client");
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            } 
+        }
+        public decimal Deposit
+        {
+            get => deposit;
+            set
+            {
+                deposit = value;
+                OnPropertyChanged("Deposit");
+            }
+        }
+        public decimal InterestRate
+        {
+            get => interestRate;
+            set
+            {
+                interestRate = value;
+                OnPropertyChanged("InterestRate");
+            }
+        }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
